f464d46b83eca34303a1b0ac53c7ea582beff756
mijail pulgar <mpulgar@abatechnology.com>
2025-01-31 10:31:58 -0500

    feat: health check

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,8 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
+  console.log('health check configured');
+
   logger.log(`Payments Microservice running on port ${envs.port}`);
 }
 bootstrap();
